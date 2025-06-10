97fd5f23f55a9b9ecb059b30f26988b8da7deba0
SaiSumakar <saisumakar05082005@gmail.com>
2025-06-10 20:24:53 +0530

    update files

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",  // Adjust base path for GitHub Pages
+  base: "/portfolio-website/",  // Adjust base path for GitHub Pages
 })
