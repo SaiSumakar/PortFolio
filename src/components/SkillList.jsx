5b9666275a8c9957593c71c4ce0a7fa927f01736
sainatha-reddy <sainathreddy057@gmail.com>
2025-06-10 22:20:54 +0530

    Update SkillList.jsx

--- src/components/SkillList.jsx
+++ src/components/SkillList.jsx
@@ -1,22 +1,22 @@
 import PropTypes from "prop-types";
 
 const langsImages = [
-  { name: "C", path: "/assets/c.png" },
-  { name: "C++", path: "/assets/cpp.png" },
-  { name: "Python", path: "/assets/python.png" },
-  { name: "HTML", path: "/assets/html.png" },
-  { name: "JavaScript", path: "/assets/js.png" },
+  { name: "C", path: "./assets/c.png" },
+  { name: "C++", path: "./assets/cpp.png" },
+  { name: "Python", path: "./assets/python.png" },
+  { name: "HTML", path: "./assets/html.png" },
+  { name: "JavaScript", path: "./assets/js.png" },
 ];
 
 const fworksImages = [
-  { name: "CSS", path: "/assets/css.png" },
-  { name: "PyTorch", path: "/assets/pytorch.png" },
-  { name: "TensorFlow", path: "/assets/tf.png" },
-  { name: "NumPy", path: "/assets/numpy.png" },
-  { name: "pandas", path: "/assets/pandas.png" },
-  { name: "Matplotlib", path: "/assets/plt.png" },
-  { name: "Node.js", path: "/assets/nodejs.png" },
-  { name: "React.js", path: "/assets/react.png" },
+  { name: "CSS", path: "./assets/css.png" },
+  { name: "PyTorch", path: "./assets/pytorch.png" },
+  { name: "TensorFlow", path: "./assets/tf.png" },
+  { name: "NumPy", path: "./assets/numpy.png" },
+  { name: "pandas", path: "./assets/pandas.png" },
+  { name: "Matplotlib", path: "./assets/plt.png" },
+  { name: "Node.js", path: "./assets/nodejs.png" },
+  { name: "React.js", path: "./assets/react.png" },
 ];
 
 function displayList(list, typeSkill) {
