57d697d7e12c6287fde39e97650024c8ad28ee60
sainatha-reddy <sainathreddy057@gmail.com>
2025-06-10 22:50:08 +0530

    Update SkillList.jsx

--- src/components/SkillList.jsx
+++ src/components/SkillList.jsx
@@ -1,72 +1,73 @@
 import PropTypes from "prop-types";
 
-const langsImages = [
-  { name: "C", path: "./assets/c.png" },
-  { name: "C++", path: "./assets/cpp.png" },
-  { name: "Python", path: "./assets/python.png" },
-  { name: "HTML", path: "./assets/html.png" },
-  { name: "JavaScript", path: "./assets/js.png" },
-];
+const skills = {
+  languages: [
+    { name: "C", path: "./assets/c.png" },
+    { name: "C++", path: "./assets/cpp.png" },
+    { name: "Python", path: "./assets/python.png" },
+    { name: "HTML", path: "./assets/html.png" },
+    { name: "JavaScript", path: "./assets/js.png" },
+  ],
+  fworks: [
+    { name: "CSS", path: "./assets/css.png" },
+    { name: "PyTorch", path: "./assets/pytorch.png" },
+    { name: "TensorFlow", path: "./assets/tf.png" },
+    { name: "NumPy", path: "./assets/numpy.png" },
+    { name: "pandas", path: "./assets/pandas.png" },
+    { name: "Matplotlib", path: "./assets/plt.png" },
+    { name: "Node.js", path: "./assets/nodejs.png" },
+    { name: "React.js", path: "./assets/react.png" },
+  ]
+};
 
-const fworksImages = [
-  { name: "CSS", path: "./assets/css.png" },
-  { name: "PyTorch", path: "./assets/pytorch.png" },
-  { name: "TensorFlow", path: "./assets/tf.png" },
-  { name: "NumPy", path: "./assets/numpy.png" },
-  { name: "pandas", path: "./assets/pandas.png" },
-  { name: "Matplotlib", path: "./assets/plt.png" },
-  { name: "Node.js", path: "./assets/nodejs.png" },
-  { name: "React.js", path: "./assets/react.png" },
-];
+const displayList = (list, type) => {
+  const baseDelay = type === "languages" ? 25 : 60;
 
-function displayList(list, typeSkill) {
-  return list.map((element, index) => {    
-    const num = typeSkill === "languages" ? 25 : 60;
-    const delay = `calc(${num}s / ${list.length} * (${list.length} - ${index}) * -1)`;
+  return list.map((el, i) => {
+    const delay = `calc(${baseDelay}s / ${list.length} * (${list.length} - ${i}) * -1)`;
     return (
-      <div 
-        key={element.name} 
-        className={`skill-div ${element.name.replace("+", "p").toLowerCase()} ${typeSkill}`}
-        data-content={element.name}
-        style={{animationDelay: delay}}
+      <div
+        key={el.name}
+        className={`skill-div ${el.name.replace("+", "p").toLowerCase()} ${type}`}
+        data-content={el.name}
+        style={{ animationDelay: delay }}
       >
-        <img src={element.path} draggable="false" className="skill" style={{WebkitUserSelect: 'none'}}/>
+        <img
+          src={el.path}
+          alt={el.name}
+          draggable="false"
+          className="skill"
+          style={{ WebkitUserSelect: "none" }}
+        />
       </div>
-    )
-  })
-}
+    );
+  });
+};
 
-function SkillList(props) {
-  const lview = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  }
+function SkillList({ type }) {
+  const viewStyles = {
+    languages: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+    },
+    fworks: {
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
+      rowGap: "2.5rem",
+    },
+  };
 
-  const fview = {
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 1fr)",
-    rowGap: "2.5rem",
-  }
-
- if (props.type === "languages") {
-    return (
-      <div id="languages-list" style={lview}>
-        {displayList(langsImages, props.type)}
-      </div>
-    )
-  } else if (props.type === "fworks") {
-    return (
-      <div id="fworks-list" style={fview}>
-        {displayList(fworksImages, props.type)}
-      </div>
-    )
-  }
+  return (
+    <div id={`${type}-list`} style={viewStyles[type]}>
+      {displayList(skills[type], type)}
+    </div>
+  );
 }
 
 SkillList.propTypes = {
-  type: PropTypes.string.isRequired
-}
+  type: PropTypes.oneOf(["languages", "fworks"]).isRequired,
+};
 
 export default SkillList;
