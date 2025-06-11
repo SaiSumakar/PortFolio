2d97aa88ff9f6669a2dfaefda590f50c59140fed
SaiSumakar <saisumakar05082005@gmail.com>
2025-06-11 11:17:50 +0530

    add paths file

---
+++ src/components/image_paths.js
@@ -0,0 +1,36 @@
+import cimg from "/assets/c.png";
+import cppimg from "/assets/cpp.png";
+import pythonimg from "/assets/python.png";
+import htmlimg from "/assets/html.png";
+import jsimg from "/assets/js.png";
+import cssimg from "/assets/css.png";
+import pytorchimg from "/assets/pytorch.png";
+import tfimg from "/assets/tf.png";
+import numpyimg from "/assets/numpy.png";
+import pandasimg from "/assets/pandas.png";
+import pltimg from "/assets/plt.png";
+import nodejsimg from "/assets/nodejs.png";
+import reactimg from "/assets/react.png";
+
+
+const skills = {
+  languages: [
+    { name: "C", path: cimg },
+    { name: "C++", path: cppimg },
+    { name: "Python", path: pythonimg },
+    { name: "HTML", path: htmlimg  },
+    { name: "JavaScript", path: jsimg },
+  ],
+  fworks: [
+    { name: "CSS", path: cssimg },
+    { name: "PyTorch", path: pytorchimg },
+    { name: "TensorFlow", path: tfimg },
+    { name: "NumPy", path: numpyimg },
+    { name: "pandas", path: pandasimg},
+    { name: "Matplotlib", path: pltimg },
+    { name: "Node.js", path: nodejsimg },
+    { name: "React.js", path: reactimg },
+  ]
+};
+
+export default { skills };
